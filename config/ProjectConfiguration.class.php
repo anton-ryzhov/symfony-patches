c0d311bb96d715e875ec8361bf8c4ff2bfb8fc47
Anton Ryzhov <anton@ryzhov.me>
2011-06-12 15:01:31 +0400

    PREPARE: New project on latest version of symfony 1.4 and doctrine1 http://svn.symfony-project.com/branches/1.4/ Revision 32641 https://github.com/doctrine/doctrine1 commit c6bbf677f8d0f07ed971e4643e796850a0c62aaa

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once realpath(dirname(__FILE__)).'/../lib/symfony/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
